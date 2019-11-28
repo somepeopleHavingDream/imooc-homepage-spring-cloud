e7d57b5bda7413b0fce8f0f91207feb5c5b5c0bc
yangxin <1393003255@qq.com>
2019-11-28 17:43:40 +0800

    feat: 服务网关模块的开发

---
+++ homepage-zuul/src/main/java/org/yangxin/homepagezuul/ZuulGatewayApplication.java
@@ -0,0 +1,21 @@
+package org.yangxin.homepagezuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * 网关应用程序
+ * EnableZuulProxy：标识当前的应用是Zuul Server
+ * SpringCloudApplication：用于简化配置的组合注解
+ *
+ * @author yangxin
+ * 2019/11/28 17:23
+ */
+@EnableZuulProxy
+@SpringCloudApplication
+public class ZuulGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulGatewayApplication.class, args);
+    }
+}
