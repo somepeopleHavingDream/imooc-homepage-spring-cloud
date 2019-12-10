f186848b49b137346ec1abc066c4c7b35939b761
yangxin <1393003255@qq.com>
2019-12-10 14:43:58 +0800

    feat: 测试对外服务接口的可用性

--- imooc-homepage-service/homepage-course/src/main/java/org/yangxin/homepage/controller/HomepageCourseController.java
+++ imooc-homepage-service/homepage-course/src/main/java/org/yangxin/homepage/controller/HomepageCourseController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.yangxin.homepage.service.ICourseService;
-import org.yangxin.imoochomepage.vo.CourseInfoVO;
 import org.yangxin.imoochomepage.request.CourseInfoRequest;
+import org.yangxin.imoochomepage.vo.CourseInfoVO;
 
 import java.util.Collections;
 import java.util.List;
